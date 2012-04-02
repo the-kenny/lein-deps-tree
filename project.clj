22474ea55727b2bbd6fe5668d557c6ba1a4c5844
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-04-02 14:28:29 +0200

    add github-url as :url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-deps-tree "0.1.0"
   :description "Prints all dependencies formatted as a tree"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/the-kenny/lein-deps-tree"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
